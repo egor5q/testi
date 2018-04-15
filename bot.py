42dd8136b1f0c73e91e17fbdf9ac392bd530a476
egor5q <31898567+egor5q@users.noreply.github.com>
2018-04-15 13:54:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,14 +64,14 @@ def elit(m):
         bot.send_message(m.from_user.id, 'Вы элита!', reply_markup=Kb)
     
     
-@bot.message_handler(commands=['update'])
-def upd(m):
-  if m.from_user.id==441399484:
-         try:
-            iduser.update_many({'pet':{'$ne':None}}, {'$set':{'pet.lose':0}})
-
-         except:
-            pass
+#@bot.message_handler(commands=['update'])
+#def upd(m):
+#  if m.from_user.id==441399484:
+#         try:
+#            iduser.update_many({'pet':{'$ne':None}}, {'$set':{'pet.lose':0}})
+#
+#         except:
+#            pass
             
             
 @bot.message_handler(commands=['mysize'])
