1772bc1b945983c7ec5b5f2aa756587881e90d32
egor5q <31898567+egor5q@users.noreply.github.com>
2018-05-14 16:07:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -448,8 +448,8 @@ def xod(id1, id2, name1, name2, player1, player2):
   Keyboard.add(types.InlineKeyboardButton(text='Окончить выбор', callback_data='endattack'))
   msg1=bot.send_message(id1, 'Теперь выставьте количество атаки, которое хотите поставить в этом ходу. Текущая атака: 0', reply_markup=Keyboard)  
   msg2=bot.send_message(id2, 'Теперь выставьте количество атаки, которое хотите поставить в этом ходу. Текущая атака: 0', reply_markup=Keyboard)
-  id1['message']=msg1.message_id
-  id2['message']=msg2.message_id
+  ids['id1']['message']=msg1.message_id
+  ids['id2']['message']=msg2.message_id
     
 
 def ready(ids, id2, game): 
