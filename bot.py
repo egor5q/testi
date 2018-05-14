2127ffd7a7853f762a09d7cb2c14fab9dd270cc0
egor5q <31898567+egor5q@users.noreply.github.com>
2018-05-14 15:47:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -113,26 +113,8 @@ def mme(m):
         bot.send_message(m.chat.id, 'Упс! Какая-то ошибка! Наверное, вы ни разу не измеряли член! (напишите боту "член")')
         bot.send_message(441399484, 'Упс! Какая-то ошибка! Наверное, вы ни рару не измеряли член!')                                                                                                                               
                                                                  
-                                                                  
-@bot.message_handler(commands=['challenge'])
-def challenge(m):
-    if m.from_user.id==441399484:
-      if len(ch)<1:
-        bot.send_message(m.chat.id, 'Конкурс за приз - кнопки внутри членомера - начинается! Жмите /joen для присоединения!')
-        ch.append(m.chat.id)
-                
-                
-
-@bot.message_handler(commands=['joen'])
-def joen(m):
-    if m.chat.id in ch and m.from_user.id not in members:
-        if m.chat.id==ch[0]:
-            members.append(m.from_user.id)
-            
-            
-@bot.message_handler(commands=['begin'])
-def begin(m):
-    pass
+        
+          
                 
 @bot.message_handler(commands=['channel'])
 def channel(message):
@@ -141,7 +123,7 @@ def channel(message):
 
 @bot.message_handler(commands=['start'])
 def startms(message):
-    bot.send_message(message.from_user.id, 'Если ты здесь, то ты наверняка хочешь измерить член! Пиши /commands, чтобы узнать, на какие слова реагирует бот')
+    bot.send_message(message.from_user.id, 'Это бот для сражений питомцев Членомера! Подробнее - /pethelp')
 
 
 @bot.message_handler(commands=['info'])
@@ -381,10 +363,10 @@ def gofight(id1, id2, name1, name2):
     player2['pet']['attack']=player2['pet']['maxattack']
     player2['pet']['defence']=player2['pet']['maxdefence']
     play.append(creategame(id1, id2, player1, player2))
-    bot.send_message(id1, 'Битва начинается! Ваш питомец дерётся с питомцем, которого зовут '+'"'+name2+'"'+'! Его уровень: '+str(player2['pet']['level']))
-    bot.send_message(id2, 'Битва начинается! Ваш питомец дерётся с питомцем, которого зовут '+'"'+name1+'"'+'! Его уровень: '+str(player1['pet']['level']))
-    bot.send_message(441399484, 'Битва начинается! Ваш питомец дерётся с питомцем, которого зовут '+'"'+name2+'"'+'! Его уровень: '+str(player2['pet']['level']))
-    bot.send_message(441399484, 'Битва начинается! Ваш питомец дерётся с питомцем, которого зовут '+'"'+name1+'"'+'! Его уровень: '+str(player1['pet']['level']))
+    bot.send_message(id1, 'Битва начинается! Ваш питомец дерётся с питомцем, которого зовут '+'*"'+name2+'"'+'*! Его уровень: '+str(player2['pet']['level']), parse_mode='markdown')
+    bot.send_message(id2, 'Битва начинается! Ваш питомец дерётся с питомцем, которого зовут '+'*"'+name1+'"'+'*! Его уровень: '+str(player1['pet']['level']), parse_mode='markdown')
+    bot.send_message(441399484, 'Битва начинается! Ваш питомец дерётся с питомцем, которого зовут '+'*"'+name2+'"'+'*! Его уровень: '+str(player2['pet']['level']), parse_mode='markdown')
+    bot.send_message(441399484, 'Битва начинается! Ваш питомец дерётся с питомцем, которого зовут '+'*"'+name1+'"'+'*! Его уровень: '+str(player1['pet']['level']), parse_mode='markdown')
     xod(id1, id2, name1, name2, player1, player2)
     
     
@@ -492,18 +474,18 @@ def endturn(game):############################################################# 
     if losehp1<0:
         losehp1=0
     player1['hp']-=losehp1
-    text1+=player1['name']+':\n'+'Выставленная атака: '+str(player1['attackround'])+'\nВыставленная защита: '+str(player1['defenceround'])+'\nПолученный урон: '+str(losehp1)
+    text1+='*'+player1['name']+'*:\n'+'Выставленная атака: '+str(player1['attackround'])+'\nВыставленная защита: '+str(player1['defenceround'])+'\nПолученный урон: '+str(losehp1)
     
         
     losehp2=damage1-defence2
     if losehp2<0:
         losehp2=0
     player2['hp']-=losehp2
-    text2+=player2['name']+':\n'+'Выставленная атака: '+str(player2['attackround'])+'\nВыставленная защита: '+str(player2['defenceround'])+'\nПолученный урон: '+str(losehp2)
+    text2+='*'+player2['name']+'*:\n'+'Выставленная атака: '+str(player2['attackround'])+'\nВыставленная защита: '+str(player2['defenceround'])+'\nПолученный урон: '+str(losehp2)
         
         
-    bot.send_message(player1['id'], 'Результаты хода:\n\n'+text1+'\n'+text2)
-    bot.send_message(player2['id'], 'Результаты хода:\n\n'+text1+'\n'+text2)
+    bot.send_message(player1['id'], 'Результаты хода:\n\n'+text1+'\n'+'\n'+text2, parse_mode='markdown')
+    bot.send_message(player2['id'], 'Результаты хода:\n\n'+text1+'\n'+'\n'+text2, parse_mode='markdown')
     player1['attackround']=0
     player1['defenceround']=0
     player1['ready']=0
@@ -555,12 +537,12 @@ def buypet(m):
     x=iduser.find_one({'id':m.from_user.id})
     if x!=None:
       if x['pet']==None:
-        if x['chlenocoins']>=25:
+        if x['chlenocoins']>=5:
             iduser.update_one({'id':m.from_user.id}, {'$set':{'pet':petcreate()}})
-            iduser.update_one({'id':m.from_user.id}, {'$inc':{'chlenocoins':-25}})
+            iduser.update_one({'id':m.from_user.id}, {'$inc':{'chlenocoins':-5}})
             bot.send_message(m.chat.id, 'Поздравляю, вы купили питомца! Подробнее об этом в /pethelp.')
         else:
-            bot.send_message(m.chat.id, 'Не хватает членокоинов! (нужно 25)')
+            bot.send_message(m.chat.id, 'Не хватает членокоинов! (нужно 5)')
       else:
         bot.send_message(m.chat.id, 'У вас уже есть питомец!')
     else:
@@ -581,118 +563,12 @@ def pethelp(m):
                              
                              
                              
-@bot.message_handler(commands=['commands'])
-def commessage(message):
-    bot.send_message(message.chat.id, 'Все фразы, связанные со словом "член"')
-        
 @bot.message_handler(commands=['feedback'])
 def feedback(message):
     if message.from_user.username!=None:
       bot.send_message(314238081, message.text+"\n"+'@'+message.from_user.username)
       bot.send_message(message.chat.id, 'Сообщение отправлено!')
 
-
-texts=['Как у коня', '5000км! Мужик!', '1 миллиметр... В стоячем состоянии',
-      'Ваши яйца поглотили член', 'Ваш член разбил мультивселенную', 'Член в минусе', 'Ваш писюн не даёт себя измерить',
-       'Член в астрале', 'Прислоните член к экрану, я не вижу'
-      ]
-
-@bot.message_handler(content_types=['text'])
-def chlenomer(message):
-  if message.from_user.id not in ban:
-    if message.chat.id<0:
-      if idgroup.find_one({'id':message.chat.id}) is None:
-        idgroup.insert_one({'id':message.chat.id})
-      if iduser.find_one({'id':message.from_user.id}) is None:
-            iduser.insert_one({'id':message.from_user.id, 'summ':0, 'kolvo':0, 'chlenocoins':0, 'pet':None})
-    elif message.chat.id>0:
-        if iduser.find_one({'id':message.from_user.id}) is None:
-            iduser.insert_one({'id':message.from_user.id, 'summ':0, 'kolvo':0, 'chlenocoins':0, 'pet':None})
-                                          
-    
-    if 'член' in message.text.lower() or 'хер' in message.text.lower() or 'хуй' in message.text.lower() or 'залупа' in message.text.lower() or 'пиписька' in message.text.lower() or 'пенис' in message.text.lower() or 'хуе' in message.text.lower() or 'хуё' in message.text.lower():
-        print(message.chat.id)
-        mega=random.randint(1,100)
-        ultramega=random.randint(1,1000)
-        hyperultramega=random.randint(1, 10000)
-        win=random.randint(1, 100000)
-        chlen=random.randint(1,100)
-        mm=random.randint(0,9)
-        randomvoice=random.randint(1,100)
-        t=0
-        if randomvoice>90:
-              chlen = random.randint(1, 6)
-              text=texts[chlen-1]
-              t=1
-        else:
-            replytext='Размер члена '+message.from_user.first_name+': '+str(chlen)+','+str(mm)+' см'
-            bot.send_message(message.chat.id, replytext)
-            otvet=chlen+mm/10
-            iduser.update_one({'id':message.from_user.id}, {'$inc':{'kolvo':1}})
-            iduser.update_one({'id':message.from_user.id}, {'$inc':{'summ':otvet}})
-        if mega==1:
-            iduser.update_one({'id':message.from_user.id}, {'$inc':{'chlenocoins':1}})
-            text='Вы нашли секретное сообщение, шанс которого 1%!'+"\n"+'Есть еще секретные сообщения, шанс которых еще ниже...\nК тому же, вы получили 1 членокоин! Смотрите /me для проверки.'
-            t=1
-        if ultramega==1:
-            iduser.update_one({'id':message.from_user.id}, {'$inc':{'chlenocoins':7}})
-            text='Вы нашли СУПЕР-СЕКРЕТНОЕ сообщение, шанс которого равен 0,1%!'+"\n"+'А ведь есть БОЛЕЕ секретные сообщения...\nК тому же, вы получили 7 членокоинов! Смотрите /me для проверки.'
-            t=1
-        if hyperultramega==1:
-            iduser.update_one({'id':message.from_user.id}, {'$inc':{'chlenocoins':15}})
-            text='Поздравляю, вы нашли УЛЬТРА секретное сообщение, шанс которого равен 0,01%!'+"\n"+'Это предпоследнтй уровень секретности...\nК тому же, вы получили 15 членокоинов! Смотрите /me для проверки.'
-            t=1
-            
-        if win==1:
-            iduser.update_one({'id':message.from_user.id}, {'$inc':{'chlenocoins':50}})
-            text='ВЫ ОЧЕНЬ ВЕЗУЧИЙ ЧЕЛОВЕК! Вы открыли САМОЕ СЕКРЕТНОЕ СООБЩЕНИЕ, шанс которого равен 0,001%!\nК тому же, вы получили 50 членокоинов! Смотрите /me для проверки.'
-            t=1
-        if t==1:
-            bot.send_message(message.chat.id, message.from_user.first_name+', '+text)
-            t=0
-        
-
- 
-def creategame(id1, id2, player1, player2):
-            return{
-                'id1':{'id':id1,
-                       'timer':None,
-                       'attackselect':0,
-                       'defenceselect':0,
-                       'maxattack':player1['pet']['maxattack'],
-                       'maxdefence':player1['pet']['maxdefence'],
-                       'attack':player1['pet']['maxattack'],
-                       'defence':player1['pet']['maxdefence'],
-                       'attackround':0,
-                       'defenceround':0,
-                       'ready':0,
-                       'name':player1['pet']['name'],
-                       'hp':player1['pet']['hp'],
-                       'attackregen':player1['pet']['regenattack'],
-                       'defenceregen':player1['pet']['regendefence'],
-                       'skill':player1['pet']['skill']
-                      },
-                'id2':{
-                    'timer':None,
-                    'id':id2,
-                    'attackselect':0,
-                    'defenceselect':0,
-                    'maxattack':player2['pet']['maxattack'],
-                    'maxdefence':player2['pet']['maxdefence'],
-                    'attack':player2['pet']['maxattack'],
-                    'defence':player2['pet']['maxdefence'],
-                    'attackround':0,
-                    'defenceround':0,
-                    'ready':0,
-                    'name':player2['pet']['name'],
-                    'hp':player2['pet']['hp'],
-                    'attackregen':player2['pet']['regenattack'],
-                    'defenceregen':player2['pet']['regendefence'],
-                    'skill':player2['pet']['skill']
-                     }
-            }
-            
-            
             
 def petcreate():
     return{
